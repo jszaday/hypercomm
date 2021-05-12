bb41b8d0040a7897e98da1f83c111a15bec088d4
Wingpad <wingpad@gmail.com>
2021-05-12 17:05:18 -0500

    begin integration with serialization

--- include/hypercomm/components.hpp
+++ include/hypercomm/components.hpp
@@ -1,10 +1,9 @@
 #ifndef __HYPERCOMM_COMPONENTS_HPP__
 #define __HYPERCOMM_COMPONENTS_HPP__
 
-#include "components/manager.hpp"
+#include "components/identifiers.hpp"
 #include "components/component.hpp"
-#include "components/selectors.hpp"
 #include "components/attributes.hpp"
-#include "components/registration.hpp"
+#include "components/selectors.hpp"
 
 #endif
---
+++ include/hypercomm/serialization/construction.hpp
@@ -0,0 +1,55 @@
+#ifndef __HYPERCOMM_CONSTRUCTION_HPP__
+#define __HYPERCOMM_CONSTRUCTION_HPP__
+
+#include <charm++.h>
+
+namespace hypercomm {
+
+template <typename T>
+inline typename std::enable_if<
+    std::is_constructible<T, PUP::reconstruct>::value>::type
+reconstruct(T* p) {
+  ::new (p) T(PUP::reconstruct());
+}
+
+template <typename T>
+inline typename std::enable_if<
+    std::is_constructible<T, CkMigrateMessage*>::value &&
+    !std::is_constructible<T, PUP::reconstruct>::value>::type
+reconstruct(T* p) {
+  ::new (p) T(nullptr);
+}
+
+template <typename T>
+inline typename std::enable_if<
+    !std::is_constructible<T, PUP::reconstruct>::value &&
+    !std::is_constructible<T, CkMigrateMessage*>::value &&
+    std::is_default_constructible<T>::value>::type
+reconstruct(T* p) {
+  ::new (p) T();
+}
+
+template <typename T>
+struct temporary {
+  typename std::aligned_storage<sizeof(T), alignof(T)>::type data;
+
+  temporary() {
+    reconstruct(&(this->value()));
+  }
+
+  ~temporary() {
+    value().~T();
+  }
+
+  const T& value(void) const {
+    return *(reinterpret_cast<const T*>(&data));
+  }
+
+  T& value(void) {
+    return *(reinterpret_cast<T*>(&data));
+  }
+};
+
+}
+
+#endif
