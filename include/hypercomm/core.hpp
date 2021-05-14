e7ba62c87051651f1d2a57df6751c0d0cd72aab6
Justin Szaday <wingpad@gmail.com>
2021-05-13 23:51:29 -0500

    expand semantics of callbacks

--- include/hypercomm/core.hpp
+++ include/hypercomm/core.hpp
@@ -7,6 +7,9 @@
 namespace hypercomm {
 using callback = core::callback;
 using callback_ptr = std::shared_ptr<callback>;
+
+using combiner = core::combiner;
+using combiner_ptr = std::shared_ptr<combiner>;
 }
 
 #endif
--- include/hypercomm/core/callback.hpp
+++ include/hypercomm/core/callback.hpp
@@ -5,11 +5,27 @@
 
 namespace hypercomm {
 namespace core {
-struct callback: public polymorph {
+
+template<bool MultiMsg, bool Returns>
+struct action : virtual public polymorph {
   using value_type = std::shared_ptr<CkMessage>;
 
-  virtual void send(value_type&&) = 0;
+  using return_type = typename std::conditional<Returns, value_type, void>::type;
+  using argument_type = typename std::conditional<MultiMsg, std::vector<value_type>, value_type>::type;
+
+  virtual return_type send(argument_type&&) = 0;
+
+  template<bool multi_msg_t = MultiMsg,
+           typename std::enable_if<multi_msg_t>::type>
+  return_type send(value_type&& value) {
+    argument_type list = { value };
+    return send(std::move(list));
+  }
 };
+
+using callback = action<false, false>;
+using combiner = action<true, true>;
+
 }
 }
 
