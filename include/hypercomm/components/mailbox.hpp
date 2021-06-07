7ea6c0174f3cce28cd01cc29c73d47d763c13d7a
Justin Szaday <wingpad@gmail.com>
2021-06-07 18:11:46 -0500

    Elevate Requests to Proper Listeners (#18)
    * Elevate Requests to Proper Listeners
    
    * clean up comments

--- include/hypercomm/components/mailbox.hpp
+++ include/hypercomm/components/mailbox.hpp
@@ -7,33 +7,81 @@
 
 namespace hypercomm {
 
-template<typename T>
-class mailbox: public virtual component,
-               public virtual value_source,
-               public std::enable_shared_from_this<mailbox<T>> {
+extern callback_ptr local_connector_(const component_ptr&, const component::port_type&);
+
+template <typename T>
+class mailbox : public virtual component,
+                public virtual value_source,
+                public std::enable_shared_from_this<mailbox<T>> {
  public:
   using predicate_type = std::shared_ptr<immediate_action<bool(const T&)>>;
-  using request_type = std::pair<predicate_type, callback_ptr>;
+  using action_type = callback_ptr;
+
+  class request : public component::status_listener,
+                  public std::enable_shared_from_this<request> {
+   public:
+    std::shared_ptr<component> com;
+    predicate_type pred;
+    action_type act;
+    mailbox<T>* self;
+
+    request(mailbox<T>* _1, const predicate_type& _2, const callback_ptr& _3)
+        : self(_1), pred(_2), act(_3) {}
+
+    ~request() {}
+
+    virtual void on_completion(const component&) override {
+      // TODO this may not be necessary?
+      self->pop_request(this->shared_from_this());
+    }
+
+    virtual void on_invalidation(const component&) override {
+      self->pop_request(this->shared_from_this());
+    }
+
+    inline bool matches(const T& t) { return !pred || pred->action(t); }
+
+    inline void action(value_type&& value) { act->send(std::move(value)); }
+  };
+
+  using request_type = std::shared_ptr<request>;
 
  protected:
   std::deque<request_type> requests_;
   std::deque<std::shared_ptr<typed_value<T>>> buffer_;
 
-public:
-  mailbox(const id_t& _1) : component(_1)  {}
+ public:
+  mailbox(const id_t& _1) : component(_1) {}
 
   virtual std::size_t n_inputs(void) const override { return 1; }
   virtual std::size_t n_outputs(void) const override { return 0; }
   virtual bool keep_alive(void) const override { return true; }
 
-  virtual void put_request(const predicate_type& pred, const callback_ptr& cb) {
+  inline request_type put_request(const predicate_type& pred, const callback_ptr& cb) {
     auto search = this->find_in_buffer(pred);
     if (search == std::end(this->buffer_)) {
-      this->requests_.emplace_back(pred, cb);
+      auto req = std::make_shared<request>(this, pred, cb);
+      this->requests_.emplace_back(req);
+      return req;
     } else {
       QdProcess(1);
       cb->send(std::move(*search));
       this->buffer_.erase(search);
+      return {};
+    }
+  }
+
+  inline void put_request_to(const predicate_type& pred, const component_ptr& com, const component::port_type& port) {
+    auto cb = local_connector_(com, port);
+    auto req = this->put_request(pred, cb);
+    if (req) com->add_listener(req);
+  }
+
+  inline void pop_request(const request_type& req) {
+    auto end = std::end(this->requests_);
+    auto search = std::find(std::begin(this->requests_), end, req);
+    if (search != end) {
+      this->requests_.erase(search);
     }
   }
 
@@ -50,8 +98,11 @@ public:
       QdCreate(1);
       this->buffer_.emplace_back(std::move(value));
     } else {
-      search->second->send(std::move(value));
+      auto req = *search;
       this->requests_.erase(search);
+      // delete req before sending cb to preclude
+      // feedback loops
+      req->action(std::move(value));
     }
 
     return {};
@@ -72,18 +123,18 @@ public:
 
   using request_iterator = typename decltype(requests_)::iterator;
 
-  inline request_iterator find_matching(const std::shared_ptr<typed_value<T>>& _1) {
+  inline request_iterator find_matching(
+      const std::shared_ptr<typed_value<T>>& _1) {
     const auto& value = _1->value();
     request_iterator search = std::begin(this->requests_);
     for (; search != std::end(this->requests_); search++) {
-      if (!search->first || search->first->action(value)) {
+      if ((*search)->matches(value)) {
         break;
       }
     }
     return search;
   }
 };
-
 }
 
 #endif
