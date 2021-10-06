ff6f5b16a1d53df5e9480d4df83bc49b36e914e3
Justin Szaday <wingpad@gmail.com>
2021-10-06 16:07:48 -0400

    Add Preliminary PingPong

---
+++ tests/pingpong/tester.hh
@@ -0,0 +1,12 @@
+#ifndef __TESTER_HH__
+#define __TESTER_HH__
+
+#include <hypercomm/components/mailbox.hpp>
+#include <hypercomm/core/locality.hpp>
+#include <hypercomm/core/persistent_port.hpp>
+
+#include "tester.decl.h"
+
+using namespace hypercomm;
+
+#endif
