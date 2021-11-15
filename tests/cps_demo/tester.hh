587de42c55c431e18a549b1f8b3a99bacb17b2fb
Justin Szaday <jjszaday@gmail.com>
2021-11-15 15:12:09 -0500

    Add Demo for CPS Programming

---
+++ tests/cps_demo/tester.hh
@@ -0,0 +1,12 @@
+#ifndef __TESTER_HH__
+#define __TESTER_HH__
+
+#include <hypercomm/core/locality.hpp>
+#include <hypercomm/core/typed_value.hpp>
+#include <hypercomm/components/mailbox.hpp>
+#include <hypercomm/core/persistent_port.hpp>
+
+#include "tester.decl.h"
+#include "pseudo_stack.hpp"
+
+#endif
