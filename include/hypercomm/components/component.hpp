5bb07f2c133f94a2b335e08dc3f81f01dde086fa
Justin Szaday <wingpad@gmail.com>
2021-11-17 13:46:19 -0500

    Restructure Component with Acceptor+Inbox (#119)
    * Restructure Component with Acceptor+Inbox
    
    * Fix Build Issues on CI
    
    * try again?
    
    * Lastly, Move Stage Action

--- include/hypercomm/components/component.hpp
+++ include/hypercomm/components/component.hpp
@@ -3,6 +3,7 @@
 
 #include <ck.h>
 #include "base.hpp"
+#include "inbox.hpp"
 #include "outbox.hpp"
 #include "../core/typed_value.hpp"
 
@@ -46,6 +47,10 @@ struct invoker_<std::nullptr_t> {
 
 namespace components {
 template <typename Inputs, typename Outputs>
+struct default_acceptor_;
+
+template <typename Inputs, typename Outputs,
+          template <typename, typename> class Acceptor = default_acceptor_>
 class component : public base_ {
  public:
   using in_type = typename tuplify_<Inputs>::type;
@@ -60,14 +65,18 @@ class component : public base_ {
   template <std::size_t O>
   using out_elt_t = typename std::tuple_element<O, out_set>::type;
 
+  using acceptor_type = Acceptor<Inputs, Outputs>;
+
+  friend acceptor_type;
+
  private:
   static constexpr std::size_t n_inputs_ = std::tuple_size<in_type>::value;
   static constexpr std::size_t n_outputs_ = std::tuple_size<out_type>::value;
   static_assert(n_inputs_ >= 1, "expected at least one input!");
 
  protected:
+  using incoming_type = typename acceptor_type::incoming_type;
   using outgoing_type = outbox_<out_set>;
-  using incoming_type = std::deque<in_set>;
   using incoming_iterator = typename incoming_type::iterator;
 
   incoming_type incoming_;
@@ -84,54 +93,16 @@ class component : public base_ {
   component(std::size_t id_)
       : base_(id_, n_inputs_, n_outputs_, acceptors.data(), in_types.data()) {}
 
-  virtual ~component() { this->empty_buffers_(); }
-
- protected:
-  template <std::size_t I>
-  inline static void accept_(component<Inputs, Outputs>* self,
-                             in_elt_t<I>&& val) {
-    if (!(val || self->permissive)) {
-      self->on_invalidation_<I>();
-    } else if (n_inputs_ == 1) {
-      // bypass seeking a partial set for single-input coms
-      direct_stage<I>(self, std::move(val));
-    } else {
-      // look for a set missing this value
-      auto search = self->find_gap_<I>();
-      const auto gapless = search == std::end(self->incoming_);
-      // if we couldn't find one:
-      if (gapless) {
-        // create a new set
-        self->incoming_.emplace_front();
-        // then update the search iterator
-        search = self->incoming_.begin();
-      }
-      // update the value in the set
-      std::get<I>(*search) = std::move(val);
-      // check whether it's ready, and stage it if so!
-      if (!gapless && is_ready(*search)) {
-        self->stage_action(search);
-      }
-    }
-  }
-
-  // TODO ( reject values after we've been deactivated! )
-  template <std::size_t I>
-  static bool accept_(base_* base, component_port_t port, deliverable& dev) {
-    CkAssertMsg(port < n_inputs_, "port out of range!");
-    auto* self = static_cast<component<Inputs, Outputs>*>(base);
-    accept_<I>(self, dev_conv_<in_elt_t<I>>::convert(std::move(dev)));
-    return true;
-  }
+  virtual ~component() { this->incoming_.empty_buffers(); }
 
  public:
-  template <std::size_t O, std::size_t I, typename Input_, typename Output_>
-  void output_to(const component<Input_, Output_>& peer) {
-    static_assert(
-        std::is_same<
-            out_elt_t<O>,
-            typename component<Input_, Output_>::template in_elt_t<I>>::value,
-        "component types must be compatible");
+  template <std::size_t O, std::size_t I, typename Input_, typename Output_,
+            template <typename, typename> class Acceptor_>
+  void output_to(const component<Input_, Output_, Acceptor_>& peer) {
+    static_assert(std::is_same<out_elt_t<O>,
+                               typename component<Input_, Output_, Acceptor_>::
+                                   template in_elt_t<I>>::value,
+                  "component types must be compatible");
     this->output_to<O>(peer.id, I);
   }
 
@@ -157,16 +128,16 @@ class component : public base_ {
     CkAssertMsg(!this->active, "component already online");
     this->activated = this->active = true;
     incoming_iterator search;
-    while (this->active &&
-           (search = this->find_ready_()) != std::end(this->incoming_)) {
-      this->stage_action(search);
+    while (this->active && (search = this->incoming_.find_ready()) !=
+                               std::end(this->incoming_)) {
+      acceptor_type::stage_action(this, search);
     }
   }
 
   virtual void deactivate(status_ signal) override {
     this->active = false;
 
-    this->empty_buffers_();
+    this->incoming_.empty_buffers();
     if (signal == status_::kInvalidation) {
       this->send_invalidations_();
     }
@@ -178,42 +149,7 @@ class component : public base_ {
     return this->activated && !this->active && this->outgoing_.empty();
   }
 
- protected:
-  inline static bool is_ready(const in_set& set) {
-    return is_ready_<n_inputs_ - 1>(set);
-  }
-
  private:
-  template <std::size_t I>
-  inline static void return_(in_set& set) {
-    auto& val = std::get<I>(set);
-    if (val) try_return(std::move(val));
-  }
-
-  template <std::size_t I>
-  inline typename std::enable_if<(I == 0)>::type empty_buffer_(in_set& set) {
-    return_<I>(set);
-  }
-
-  template <std::size_t I>
-  inline typename std::enable_if<(I >= 1)>::type empty_buffer_(in_set& set) {
-    this->empty_buffer_<(I - 1)>(set);
-    return_<I>(set);
-  }
-
-  void empty_buffers_(void) {
-    if (this->incoming_.empty()) {
-      return;
-    } else {
-      // try to return all unused values and
-      for (auto& set : this->incoming_) {
-        empty_buffer_<(n_inputs_ - 1)>(set);
-      }
-      // clear values so they're not used again
-      this->incoming_.clear();
-    }
-  }
-
   template <std::size_t O>
   inline typename std::enable_if<(O == 0)>::type send_invalidation_(void) {}
 
@@ -235,30 +171,22 @@ class component : public base_ {
     this->deactivate(kInvalidation);
   }
 
-  void buffer_ready_set_(in_set&& set) {
-    // this should be consistent with "find_ready"
-    // and the opposite of "find_gap"
-    this->incoming_.emplace_front(std::move(set));
-  }
-
   template <std::size_t I>
   inline static typename std::enable_if<(I == 0) && (n_inputs_ == 1)>::type
-  direct_stage(component<Inputs, Outputs>* self, in_elt_t<I>&& val) {
+  direct_stage(component<Inputs, Outputs, Acceptor>* self, in_elt_t<I>&& val) {
     CkAssertMsg((bool)val, "not equipped for invalidations!");
     in_set set(std::move(val));
     if (!self->stage_action(set, nullptr)) {
-      self->buffer_ready_set_(std::move(set));
+      // this should be consistent with "find_ready"
+      // and the opposite of "find_gap"
+      self->incoming_.emplace_front(std::move(set));
     }
   }
 
   template <std::size_t I>
   inline static typename std::enable_if<(I >= 0) && (n_inputs_ > 1)>::type
-  direct_stage(component<Inputs, Outputs>* self, in_elt_t<I>&& val) {
+  direct_stage(component<Inputs, Outputs, Acceptor>* self, in_elt_t<I>&& val) {
     CkAbort("-- unreachable --");
-  }
-
-  inline void stage_action(const incoming_iterator& search) {
-    this->stage_action(*search, [&](void) { this->incoming_.erase(search); });
   }
 
   // returns true if the set was consumed
@@ -285,56 +213,18 @@ class component : public base_ {
     }
   }
 
-  inline incoming_iterator find_ready_(void) {
-    if (!this->incoming_.empty()) {
-      for (auto it = std::begin(this->incoming_);
-           it != std::end(this->incoming_); it++) {
-        if (is_ready(*it)) {
-          return it;
-        }
-      }
-    }
-    return std::end(this->incoming_);
-  }
-
-  template <std::size_t I>
-  inline incoming_iterator find_gap_(void) {
-    if (!this->incoming_.empty()) {
-      auto search = this->incoming_.rbegin();
-      for (; search != this->incoming_.rend(); search++) {
-        if (!std::get<I>(*search)) {
-          return --search.base();
-        }
-      }
-    }
-
-    return std::end(this->incoming_);
-  }
-
-  template <std::size_t I>
-  inline static typename std::enable_if<I == 0, bool>::type is_ready_(
-      const in_set& set) {
-    return (bool)std::get<I>(set);
-  }
-
-  template <std::size_t I>
-  inline static typename std::enable_if<I >= 1, bool>::type is_ready_(
-      const in_set& set) {
-    return (bool)std::get<I>(set) && is_ready_<(I - 1)>(set);
-  }
-
   template <std::size_t I, typename Array>
   inline static typename std::enable_if<(I == 0)>::type make_acceptors_(
       Array& arr) {
     using elt_t = typename Array::value_type;
-    new (&arr[I]) elt_t(accept_<I>);
+    new (&arr[I]) elt_t(acceptor_type::template accept<I>);
   }
 
   template <std::size_t I, typename Array>
   inline static typename std::enable_if<(I >= 1)>::type make_acceptors_(
       Array& arr) {
     using elt_t = typename Array::value_type;
-    new (&arr[I]) elt_t(accept_<I>);
+    new (&arr[I]) elt_t(acceptor_type::template accept<I>);
     make_acceptors_<(I - 1), Array>(arr);
   }
 
@@ -350,18 +240,74 @@ class component : public base_ {
   }
 };
 
-#if HYPERCOMM_ERROR_CHECKING
 template <typename Inputs, typename Outputs>
-typename component<Inputs, Outputs>::in_type_array_t
-    component<Inputs, Outputs>::in_types =
-        make_type_list_<component<Inputs, Outputs>::in_type,
-                        component<Inputs, Outputs>::n_inputs_>();
+struct default_acceptor_ {
+  using component_type = component<Inputs, Outputs, default_acceptor_>;
+  using incoming_type = inbox_<typename component_type::in_set>;
+
+  template <std::size_t I>
+  using in_elt_t = typename component_type::template in_elt_t<I>;
+
+  static constexpr auto n_inputs_ = component_type::n_inputs_;
+
+ private:
+  template <std::size_t I>
+  inline static void accept_(component_type* self, in_elt_t<I>&& val) {
+    if (!(val || self->permissive)) {
+      self->template on_invalidation_<I>();
+    } else if (n_inputs_ == 1) {
+      // bypass seeking a partial set for single-input coms
+      component_type::template direct_stage<I>(self, std::move(val));
+    } else {
+      // look for a set missing this value
+      auto search = self->incoming_.template find_gap<I>();
+      const auto gapless = search == std::end(self->incoming_);
+      // if we couldn't find one:
+      if (gapless) {
+        // create a new set
+        self->incoming_.emplace_front();
+        // then update the search iterator
+        search = std::begin(self->incoming_);
+      }
+      // update the value in the set
+      std::get<I>(*search) = std::move(val);
+      // check whether it's ready, and stage it if so!
+      if (!gapless && incoming_type::is_ready(*search)) {
+        stage_action(self, search);
+      }
+    }
+  }
+
+ public:
+  // TODO ( reject values if we've been deactivated! )
+  template <std::size_t I>
+  static bool accept(base_* base, component_port_t port, deliverable& dev) {
+    CkAssertMsg(port < n_inputs_, "port out of range!");
+    auto* self = static_cast<component_type*>(base);
+    accept_<I>(self, dev_conv_<in_elt_t<I>>::convert(std::move(dev)));
+    return true;
+  }
+
+  inline static void stage_action(
+      component_type* self, const typename incoming_type::iterator& search) {
+    self->stage_action(*search, [&](void) { self->incoming_.erase(search); });
+  }
+};
+
+#if HYPERCOMM_ERROR_CHECKING
+template <typename Inputs, typename Outputs,
+          template <typename, typename> class Acceptor>
+typename component<Inputs, Outputs, Acceptor>::in_type_array_t
+    component<Inputs, Outputs, Acceptor>::in_types =
+        make_type_list_<component<Inputs, Outputs, Acceptor>::in_type,
+                        component<Inputs, Outputs, Acceptor>::n_inputs_>();
 #endif
 
-template <typename Inputs, typename Outputs>
-typename component<Inputs, Outputs>::accept_array_t
-    component<Inputs, Outputs>::acceptors =
-        component<Inputs, Outputs>::make_acceptors_<accept_array_t>();
+template <typename Inputs, typename Outputs,
+          template <typename, typename> class Acceptor>
+typename component<Inputs, Outputs, Acceptor>::accept_array_t
+    component<Inputs, Outputs, Acceptor>::acceptors =
+        component<Inputs, Outputs, Acceptor>::make_acceptors_<accept_array_t>();
 
 inline bool components::base_::accept(std::size_t port, deliverable& dev) {
 #if HYPERCOMM_STRICT_MODE
