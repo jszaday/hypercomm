d4e853f9ebec7b9dabc0b80b36134f186dfec4d9
Justin Szaday <jjszaday@gmail.com>
2022-04-01 11:03:42 -0400

    put include back

--- tests/cps_demo/tester.hh
+++ tests/cps_demo/tester.hh
@@ -4,6 +4,7 @@
 #include <hypercomm/core/locality.hpp>
 #include <hypercomm/core/typed_value.hpp>
 #include <hypercomm/core/persistent_port.hpp>
+#include <hypercomm/components/link.hpp>
 #include <hypercomm/components/mailbox.hpp>
 #include <hypercomm/components/multistate_component.hpp>
 
